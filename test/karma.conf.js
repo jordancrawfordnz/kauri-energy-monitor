5b45dd18d6d3ff57e76aa2f430a7062e4cd639de
Jordan Crawford <jordan.crawford@me.com>
2016-05-03 20:26:26 +1200

    Use AdminLTE.

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -22,11 +22,12 @@ module.exports = function(config) {
       // bower:js
       'bower_components/jquery/dist/jquery.js',
       'bower_components/angular/angular.js',
-      'bower_components/bootstrap-sass-official/assets/javascripts/bootstrap.js',
+      'bower_components/bootstrap/dist/js/bootstrap.js',
       'bower_components/angular-resource/angular-resource.js',
       'bower_components/angular-route/angular-route.js',
       'bower_components/angular-sanitize/angular-sanitize.js',
       'bower_components/angular-touch/angular-touch.js',
+      'bower_components/AdminLTE/dist/js/app.min.js',
       'bower_components/angular-mocks/angular-mocks.js',
       // endbower
       'app/scripts/**/*.js',
