9bf84f369f22032e16fe443daf0ae485b1431b5d
Jordan Crawford <jordan.crawford@me.com>
2016-06-28 21:41:31 +1200

    Tidy up state of charge logic to process all data.

--- common/models/building.js
+++ common/models/building.js
@@ -1,21 +1,11 @@
+var Promise = require('promise');
+
 module.exports = function(Building) {
+	var StateOfCharge = require('../../services/stateofcharge.js');
+
 	Building.disableRemoteMethod('__updateById__exports');
 
-	var produceStateEvery = 10*60; // ten minutes
-
-	function shouldRecordState(timeSinceLastReading, timestamp) {
-		var outBy = timestamp % produceStateEvery;
-
-		// Either the timestamp is perfectly on a record state time or the record state
-		return outBy === 0 || outBy < timeSinceLastReading;
-	};
-	
-	// Require that the state production will be the same times each day.
-	if (3600*24 % produceStateEvery !== 0) {
-		console.log('Building States should be computed by a number that divides the number of seconds in a day.')
-		process.exit();
-	}
-
+	// Gets the latest state for the building, based on the most recent timestamp.
 	Building.latestState = function(id, cb) {
 		Building.app.models.State.findOne({
 			where: {
@@ -73,107 +63,14 @@ module.exports = function(Building) {
 					}
 					var building = building.toJSON(); // make the building a pure JSON object.
 
-					// TODO: Use only one bridge for a building.
-					var bridge = building.bridges[0];
-					
-					// TODO: itterate through all pages.
-					var page = 0;
-					var amountPerPage = 100000;
 
-					// Fetch readings.
-					Reading.find({
-						order: 'timestamp asc',
-						skip: page*amountPerPage,
-						limit: amountPerPage,
-						where: {bridgeId : bridge.id}
-					}, function(getReadingsError, readings) {
-						if (getReadingsError) {
-							cb("Failed to get readings.");
-							console.log('Page: ' + page);
-							console.log(getReadingsError);
-							return;
-						}
 
-						var lastReading;
-
-							// TODO: Fill in some defaults of the state, e.g.: a default charge efficiency.
-						var state = {
-							powerIn : 0,
-							powerOut : 0,
-							batteryCapacity : 0,
-							currentChargeLevel : 0,
-							chargeEfficiency : 0.8,
-							buildingId : building.id
-						};
-						// For each reading.
-							// TODO: pull out as a seperate function.
-						readings.forEach(function(reading) {
-							var buildingPower = reading.values[building.buildingPowerSensorId];
-							var batteryVoltage = reading.values[building.batteryVoltageSensorId];
-							var batteryCurrent = reading.values[building.batteryCurrentSensorId];
-
-							var secondsSinceLastReading = 0;
-							if (lastReading) {
-								secondsSinceLastReading = reading.timestamp - lastReading.timestamp;
-							}
-
-							var powerChange = (batteryVoltage * batteryCurrent *secondsSinceLastReading) / 3600;
-							/*
-								The power change is expressed in Watt Hours.
-									A Watt Hour is 3600J.
-									The power (in Watts, P=IV) multiplied by the time in seconds gets the amount of change in Joules over the time gap.
-									Divide the number of Joules by 3600 to get the amount of change in Watt Hours.
-							*/ 
-
-							// Count the power change towards the power in and out.
-							if (powerChange > 0) { // A positive power change represents overall entry of power into the system
-								state.powerIn += powerChange;
-								state.currentChargeLevel += state.chargeEfficiency * powerChange; // the battery charge level has increased
-
-								if (state.currentChargeLevel > state.batteryCapacity) { // Expand the capacity to the current charge level.
-									state.batteryCapacity = state.currentChargeLevel;
-									Recalibration.create({
-										timestamp : reading.timestamp,
-										reason : 'BatteryCapacityHigherThan100',
-										buildingId : building.id
-									}, function(error) {
-										if (error) {
-											console.log('Error creating Recalibration.');
-											console.log(error);
-											return;
-										}
-									});
-									// TODO: Calibration?
-								}
-							} else { // A negative power change represents power leaving the system.
-								state.powerOut -= powerChange; // (double negative so adds to power out)
-
-									// TODO: This might be part of the initial calibration phase, need new changes to the algorithm.
-								if (state.currentChargeLevel + powerChange < 0) { // Increase the battery capacity when discharging below 0.
-									state.currentChargeLevel = 0;
-									state.batteryCapacity -= powerChange;
-										// TODO: Calibration?
-								} else {
-									state.currentChargeLevel += powerChange; // the battery charge level has decreased (double negative so decreases charge level).
-								}
-							}
-
-							// If the state needs to be recorded, then record it.
-							if (shouldRecordState(secondsSinceLastReading, reading.timestamp)) {
-								// Record the State.
-								state.timestamp = reading.timestamp;
-								State.create(state, function(error) {
-									if (error) {
-										console.log('Error creating State');
-										console.log(state);
-									}
-								});
-							}
-
-							lastReading = reading;
-						});
-
-						cb('Done');
+					// Process each page serially.
+					StateOfCharge.processAllReadings(building).then(function(result) {
+						cb(null, result);
+					}, function(error) {
+						console.log(error);
+						cb('An error occured while processing Readings.');
 					});
 				});
 			});
@@ -186,7 +83,8 @@ module.exports = function(Building) {
 			http: {path: '/:id/regeneratestate', verb: 'post'},
 		    accepts: [
 		    	{arg: 'id', type: 'string', required: true}
-		    ]
+		    ],
+		    returns: {type: 'object', root: true}
 		}
 	);
 
---
+++ services/stateofcharge.js
@@ -0,0 +1,228 @@
+var Promise = require('promise');
+var app = require('../server/server');
+
+var StateOfCharge = {};
+module.exports = StateOfCharge;
+
+var produceStateEvery = 10*60; // ten minutes
+
+// Require that the state production will be the same times each day.
+if (3600*24 % produceStateEvery !== 0) {
+	console.log('Building States should be computed by a number that divides the number of seconds in a day.')
+	process.exit();
+}
+
+// Returns a boolean. True if a state record should be saved.
+	// timeSinceLastReading: The number of seconds since the last reading.
+	// timestamp: The timestamp of the reading being processed.
+StateOfCharge.shouldRecordState = function(timeSinceLastReading, timestamp) {
+	var outBy = timestamp % produceStateEvery;
+
+	// Either the timestamp is perfectly on a record state time or the record state
+	return outBy === 0 || outBy < timeSinceLastReading;
+};
+
+// Creates a recalibration using the specified parameters. Returns a promise.
+StateOfCharge.recordRecalibration = function(building, timestamp, type) {
+	var Recalibration = app.models.Recalibration;
+	return new Promise(function(resolve, reject) {
+		Recalibration.create({
+			timestamp : timestamp,
+			reason : type,
+			buildingId : building.id
+		}, function(error, recalibration) {
+			if (error) {
+				reject(error);	
+			} else {
+				resolve(recalibration);
+			}
+		});
+	});
+};
+
+// Returns a template for a blank state object.
+StateOfCharge.getStateTemplate = function(building) {
+	return {
+		powerIn : 0,
+		powerOut : 0,
+		batteryCapacity : 0,
+		currentChargeLevel : 0,
+		chargeEfficiency : 0.8,
+		buildingId : building.id
+	};
+}
+
+/*
+Processes all readings.
+	building: The building to get readings for.
+	Returns: A Promise which Resolves:
+		{ numberOfRecords : [the count of records] }
+*/
+StateOfCharge.processAllReadings = function(building) {
+	var amountPerPage = 10000;
+	var Reading = app.models.Reading;
+
+	return new Promise(function(resolve, reject) {
+		// TODO: Use only one bridge for a building.
+		var bridge = building.bridges[0];
+		
+		// Determine how many pages.
+		Reading.count({
+			bridgeId : bridge.id
+		}, function(error, count) {
+			if (error) {
+				reject(error);
+			} else {
+				var totalPages = Math.ceil(count / amountPerPage);
+				processPage(building, bridge, 0, totalPages, null, StateOfCharge.getStateTemplate(building)).then(resolve, reject);
+			}
+		});
+	});
+
+	// Fetch the page's results, then process the reading.
+	function processPage(building, bridge, page, totalPages, lastReading, state) {
+		return new Promise(function(resolve, reject) {
+			// Fetch readings.
+			Reading.find({
+				order: 'timestamp asc',
+				skip: page * amountPerPage,
+				limit: amountPerPage,
+				where: {bridgeId : bridge.id}
+			}, function(error, readings) {
+				if (error) {
+					reject(error);
+				} else {
+					// Process the readings.
+					StateOfCharge.processReadingsSerially(building, readings, lastReading, state).then(function(processResult) {
+						var pageResult = {
+							numberOfRecords : readings.length
+						};
+						if (page < totalPages) {
+							// Provide details from this page to the next page.
+							processPage(building, bridge, page + 1, totalPages, processResult.lastReading, processResult.currentState).then(function(recurseResult) {
+								// Add to the result of the next page.
+								pageResult.numberOfRecords += recurseResult.numberOfRecords;
+								resolve(pageResult);
+							}, reject);
+						} else {
+							// Base case, last page so just return this page's result.
+							resolve(pageResult);
+						}
+					}, reject);
+				}
+			});
+		});
+	}
+};
+
+/*
+Process an entire array of readings serially.
+	building: The building all the readings belong to.
+	readings: The array of readings to process. The array order represents the order of processing.
+	initialLastReading: The last reading before the block of serially processed readings.
+	startState: The current state before processing readings.
+
+	Returns: A Promise which Resolves:
+		{ lastReading : [the last reading processed], currentState : [the current state after processing] }
+*/
+StateOfCharge.processReadingsSerially = function(building, readings, initialLastReading, startState) {
+	// Why serially? The state relies on the previous values state.
+		// As processReadings returns a promise, processReading could perform additional async tasks.
+
+	function runProcessReading(readingIndex, currentState) {
+		return new Promise(function(resolve, reject) {
+			// Base case.
+			if (readingIndex >= readings.length) {
+				resolve(currentState);
+			} else {
+				var currentReading = readings[readingIndex];
+				var lastReading = readings[readingIndex - 1];
+
+				// Get this result.
+				StateOfCharge.processReading(building, currentReading, lastReading, currentState).then(function(newState) {
+					// Run the next value with the result.
+					runProcessReading(readingIndex + 1, newState).then(function(newState) {
+						// The result from lower levels.
+						resolve(newState);
+					}, reject);
+				}, reject);
+			}
+		});
+	}
+
+	return runProcessReading(0, startState).then(function(finishState) {
+		return {
+			lastReading: readings[readings.length - 1],
+			currentState: finishState
+		};
+	});
+};
+
+/*
+Processes a single reading.
+	building: The building this reading belongs to.
+	reading: The reading to process.
+	lastReading: The reading immediately before this one. Undefined if this is the first reading.
+	currentState: The current state of charge (allowed to edit, should clone if don't want edited).
+	Returns: A promise. On resolve, returns the new state.
+*/
+StateOfCharge.processReading = function(building, reading, lastReading, currentState) {
+	var State = app.models.State;
+	return new Promise(function(resolve, reject) {
+		var buildingPower = reading.values[building.buildingPowerSensorId];
+		var batteryVoltage = reading.values[building.batteryVoltageSensorId];
+		var batteryCurrent = reading.values[building.batteryCurrentSensorId];
+
+		var secondsSinceLastReading = 0;
+		if (lastReading) {
+			secondsSinceLastReading = reading.timestamp - lastReading.timestamp;
+		}
+
+		var powerChange = (batteryVoltage * batteryCurrent * secondsSinceLastReading) / 3600;
+		/*
+			The power change is expressed in Watt Hours.
+				A Watt Hour is 3600J.
+				The power (in Watts, P=IV) multiplied by the time in seconds gets the amount of change in Joules over the time gap.
+				Divide the number of Joules by 3600 to get the amount of change in Watt Hours.
+		*/ 
+
+		// Count the power change towards the power in and out.
+		if (powerChange > 0) { // A positive power change represents overall entry of power into the system
+			currentState.powerIn += powerChange;
+			currentState.currentChargeLevel += currentState.chargeEfficiency * powerChange; // the battery charge level has increased
+
+			if (currentState.currentChargeLevel > currentState.batteryCapacity) { // Expand the capacity to the current charge level.
+				currentState.batteryCapacity = currentState.currentChargeLevel;
+				StateOfCharge.recordRecalibration(building, reading.timestamp, 'BatteryCapacityHigherThan100');
+				// TODO: Calibration?
+			}
+		} else { // A negative power change represents power leaving the system.
+			currentState.powerOut -= powerChange; // (double negative so adds to power out)
+
+				// TODO: This might be part of the initial calibration phase, need new changes to the algorithm.
+			if (currentState.currentChargeLevel + powerChange < 0) { // Increase the battery capacity when discharging below 0.
+				currentState.currentChargeLevel = 0;
+				currentState.batteryCapacity -= powerChange;
+					// TODO: Calibration?
+			} else {
+				currentState.currentChargeLevel += powerChange; // the battery charge level has decreased (double negative so decreases charge level).
+			}
+		}
+
+		// If the state needs to be recorded, then record it.
+		if (StateOfCharge.shouldRecordState(secondsSinceLastReading, reading.timestamp)) {
+			// Record the State.
+			currentState.timestamp = reading.timestamp;
+			State.create(currentState, function(error) {
+				if (error) {
+					reject();
+				} else {
+					resolve(currentState);
+				}
+			});
+		} else {
+			// Resolve the state immediately if it doesn't need to be saved.
+			resolve(currentState);
+		}
+	});
+};
