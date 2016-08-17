12472c344800f6832e1b616a051db348f58d3f7e
Jordan Crawford <jordan.crawford@me.com>
2016-08-17 21:04:54 +1200

    Support a basic energy flow graph.

--- app/scripts/controllers/buildingsummary.js
+++ app/scripts/controllers/buildingsummary.js
@@ -14,11 +14,13 @@ angular.module('offgridmonitoringApp')
     this.building = Building.findById({
       id : $routeParams.buildingId,
       filter : {
-        include : {'bridges' : 'sensors'}
+        include : ['energySources', {'bridges' : 'sensors'}]
       }
     });
 
     this.batteryDiagramHeight = 200;
+
+    // Gets the height for the battery level indicator.
     this.getBatteryLevelHeight = function() {
       var level = this.chargeLevel * this.batteryDiagramHeight;
       if (level > this.batteryDiagramHeight) {
@@ -56,55 +58,44 @@ angular.module('offgridmonitoringApp')
 
     // Sets up the summary page.
     function loadSummary(bridge) {
-      // Fetch the count of readings.
-      Bridge.readings.count({
-        id : bridge.id
-      }).$promise.then(function(count) {
-        _this.readingCount = count;
-      });
-
-      /* == Fetch down the current state.
-       Display:
-       - The current state of charge
-       - (maybe) Today's total energy usage
-       - The battery capacity
-       - The charge efficiency.
-      */
+      
+      // Fetch down the current state.
       Building.currentState({
         id : $routeParams.buildingId
       }).$promise.then(function(currentState) {
         _this.state = currentState;
         _this.chargeLevel = currentState.currentChargeLevel / currentState.batteryCapacity;
+        setupEnergyFlowGraph();
       });
-    
-      /* == Fetch down the latest sensor reading.
-        TODO: Handle multipule bridges per building.
-        This will just pull the latest reading which will only be for one of the bridges.
 
-        Display:
-        - all sensor readings with appropriate units for the latest reading
-        - the time of the latest reading
-      */
-      // Get the latest reading for the bridge.
-      Bridge.latestReading({
-        id : bridge.id
-      }).$promise.then(function(reading) {
-        _this.sensorReadings = [];
-        // Pair up the reading and its metadata.
-        angular.forEach(bridge.sensors, function(sensor) {
-          var sensorValue = reading.values[sensor.id];
-          
-          // If there is a value for this sensor ID, add the reading and metadata.
-          if (sensorValue !== undefined) {
-            _this.sensorReadings.push({
-              value : sensorValue,
-              metadata : sensor,
-              typedata : SensorTypes[sensor.type]
-            });
-          }
-        });
-        _this.latestReadingTimestamp = reading.timestamp;
+      // TODO: Get 24 hour state data too.
+    }
+
+    // Sets up the energy flow graph using the state and building data.
+    function setupEnergyFlowGraph() {
+      _this.energyFlowLabels = [];
+      _this.energyFlowData = [];
+
+      // Add consumption data.
+      _this.energyFlowLabels.push('Consumption');
+      _this.energyFlowData.push(_this.state.consumption.averagePower);
+
+      // Add energy source data.
+      _this.energyFlowLabels.push('Generator');
+      _this.energyFlowData.push(_this.state.sources.charger.averagePower);
+
+      // Add custom sources.
+      angular.forEach(_this.building.energySources, function(energySource) {
+        var sourceState = _this.state.sources[energySource.id];
+        if (sourceState) {
+          _this.energyFlowLabels.push(energySource.name);
+          _this.energyFlowData.push(sourceState.averagePower);
+        }
       });
+
+      // Add other source.
+      _this.energyFlowLabels.push(_this.building.otherEnergySourceName);
+      _this.energyFlowData.push(_this.state.sources.other.averagePower);
     }
 
   });
