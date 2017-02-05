23653956ad89c5a679c536eff6053d2006670b19
Jordan Crawford <jordan.crawford2@powershop.co.nz>
2017-02-05 15:56:25 +1300

    Support data re-processing and energy flow options.

--- app/scripts/controllers/processingconfig.js
+++ app/scripts/controllers/processingconfig.js
@@ -7,7 +7,9 @@
  * # ProcessingConfigCtrl
  * A tab for the building configuration page to allow users to set data processing options.
  */
-angular.module('offgridmonitoringApp').controller('ProcessingConfigCtrl', function ($rootScope, $scope, Building, SensorTypes) {
+angular.module('offgridmonitoringApp').controller('ProcessingConfigCtrl', function ($rootScope, $scope, Building, SensorTypes, $interval) {
+  $scope.dateTimeFormat = $rootScope.dateTimeFormat;
+
   $scope.datePickerOptions = {
     icons : {
       next: 'glyphicon glyphicon-arrow-right',
@@ -15,7 +17,7 @@ angular.module('offgridmonitoringApp').controller('ProcessingConfigCtrl', functi
       up: 'glyphicon glyphicon-arrow-up',
       down: 'glyphicon glyphicon-arrow-down'
     },
-    format : $rootScope.dateTimeFormat,
+    format : $scope.dateTimeFormat,
     timeZone : 'Pacific/Auckland'
   };
 
@@ -24,6 +26,7 @@ angular.module('offgridmonitoringApp').controller('ProcessingConfigCtrl', functi
   $scope.onlyProcessAfterObject = null;
   $scope.onlyProcessUntilObject = null;
 
+  // === Event Watchers ===
   $scope.$watch('building.onlyProcessAfterObject', function() {
     if ($scope.building.onlyProcessAfter) {
       $scope.onlyProcessAfterObject = moment.unix($scope.building.onlyProcessAfter);
@@ -40,12 +43,54 @@ angular.module('offgridmonitoringApp').controller('ProcessingConfigCtrl', functi
     }
   });
 
+  $scope.$watch('regenerationStatus.statesAreRegenerating', function() {
+    if ($scope.regenerationStatus.statesAreRegenerating && !$scope.automaticallyRefreshGenerationStatus) {
+      // Start automatically refreshing the re-generation status.
+      $scope.automaticallyRefreshGenerationStatus = $interval(function() {
+        Building.findById({
+          id : $scope.building.id
+        }, function(refreshedBuilding) {
+          $scope.regenerationStatus = {
+            statesAreRegenerating : refreshedBuilding.statesAreRegenerating,
+            lastRegeneration : refreshedBuilding.lastRegeneration
+          };
+        });
+      }, 10*1000);
+    } else if (!$scope.regenerationStatus.statesAreRegenerating && $scope.automaticallyRefreshGenerationStatus) {
+      // Cancel the automatic refreshing.
+      $interval.cancel($scope.automaticallyRefreshGenerationStatus);
+      $scope.automaticallyRefreshGenerationStatus = null;
+    }
+  });
+
+  // Stop automatically refreshing for state change.
+  $scope.$on('$destroy', function() {
+    if ($scope.automaticallyRefreshGenerationStatus) {
+      $interval.cancel($scope.automaticallyRefreshGenerationStatus);
+    }
+  });
+
+  // === Data Setup ===
   $scope.bridgesWithSensors = Building.bridges({
     id: $scope.building.id,
     filter : {
       include : ['sensors']
     }
   });
+
+  $scope.regenerationStatus = {
+    statesAreRegenerating : $scope.building.statesAreRegenerating,
+    lastRegeneration : $scope.building.lastRegeneration
+  }
+
+  // === UI Commands ===
+  $scope.regenerateStates = function() {
+    Building.regenerateState({
+      id : $scope.building.id
+    }).$promise.then(function() {
+      $scope.regenerationStatus.statesAreRegenerating = true;
+    });
+  };
 
   $scope.saveProcessingOptions = function() {
     // Update the 'onlyProcessAfter' and 'onlyProcessUntil' values from the moment objects.
@@ -65,6 +110,10 @@ angular.module('offgridmonitoringApp').controller('ProcessingConfigCtrl', functi
   };
 
   $scope.saveBatteryOptions = function() {
+    $scope.saveBuilding();
+  };
+
+  $scope.saveEnergyFlowOptions = function() {
     $scope.saveBuilding();
   };
 });
