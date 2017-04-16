95ef032ebe6c7d8e4364929ed066a4ccd6270d5b
Jordan Crawford <jordan@crawford.kiwi>
2017-04-16 16:43:30 +1200

    Output the current command in the Ubuntu install script and remove test dependencies from the frontend.

--- ubuntu-install.sh
+++ ubuntu-install.sh
@@ -1,35 +1,35 @@
 # === Tested with Ubuntu 16.04.
 # Run this from within the kauri-energy-monitor directory.
 
-# Install NodeJS.
+echo Install NodeJS.
 curl -sL https://deb.nodesource.com/setup_7.x | sudo -E bash -
 sudo apt-get install -y nodejs
 
-# Install Ruby.
+echo Install Ruby.
 sudo apt-get install -y ruby-full
 
-# Install and start MongoDB.
+echo Install and start MongoDB.
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 0C49F3730359A14518585931BC711F9BA15703C6
 echo "deb [ arch=amd64,arm64,ppc64el,s390x ] http://repo.mongodb.com/apt/ubuntu xenial/mongodb-enterprise/3.4 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-enterprise.list
 sudo apt-get update
 sudo apt-get install -y mongodb-enterprise
 sudo service mongod start
 
-# Install Compass.
+echo Install Compass.
 sudo apt-get install -y build-essential bison openssl libreadline6 libreadline6-dev curl git-core zlib1g zlib1g-dev libssl-dev libyaml-dev libxml2-dev autoconf libc6-dev ncurses-dev automake libtool
 gem install compass
 
-# Install global dependencies.
+echo Install global dependencies.
 npm install -g pm2 nodemon bower grunt
 
-# Install API dependencies.
+echo Install API dependencies.
 npm run setup
 
-# Install frontend dependencies.
+echo Install frontend dependencies.
 npm --prefix frontend run setup
 
-# Build the frontend.
+echo Build the frontend.
 grunt --base frontend --gruntfile frontend/Gruntfile.js build
 
-# Start Kauri!
+echo Start Kauri!
 pm2 start pm2.json
